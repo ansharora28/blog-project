02c6aef60f7f16c1a86cc7dc8d7aa2611310c3df
ansharora28 <ansharorake2@gmail.com>
2023-03-22 00:20:44 +0530

    swiper slider for popular section

--- components/section3.js
+++ components/section3.js
@@ -16,9 +16,9 @@ export default function section3() {
                     slidesPerView={2}
                 >
                     <SwiperSlide>{Post()}</SwiperSlide>
+                    <SwiperSlide>{Post1()}</SwiperSlide>
                     <SwiperSlide>{Post()}</SwiperSlide>
-                    <SwiperSlide>{Post()}</SwiperSlide>
-                    <SwiperSlide>{Post()}</SwiperSlide>
+                    <SwiperSlide>{Post1()}</SwiperSlide>
 
                 </Swiper>
             </div>
@@ -41,6 +41,27 @@ function Post() {
                 </div>
                 <p className="text-white-500 py-3 font-weight: 700 ">
                     This is a blog project made using NextJS and Tailwind CSS. The page is still under construction!                </p>
+                <Author></Author>
+            </div>
+        </div>
+    )
+}
+function Post1() {
+    return (
+        <div className="news">
+            <div className="image">
+                <Link href={"/"}><Image src={"/images/img4.png"} width={260} height={150} /></Link>
+            </div>
+            <div className="info flex justify-center flex-col">
+                <div className="cat">
+                    <Link href={"/"} className="text-orange-400 font-bold hover:text-green-400">NEWS</Link>
+                </div>
+                <div className="title">
+                    <Link href={"/"} className="text-white text-3xl font-bold hover:text-orange-400">You can slide these panels</Link>
+                </div>
+                <p className="text-white-500 py-3 font-weight: 700 ">
+                    I have used the swiper slider component framework to make mobile navigation easier
+                </p>
                 <Author></Author>
             </div>
         </div>
