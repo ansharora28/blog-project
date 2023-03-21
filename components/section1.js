3c4db5ce60377217fe45e53d866c477b77997ec0
ansharora28 <ansharorake2@gmail.com>
2023-03-21 15:59:08 +0530

    set up slider+autoplay

--- components/section1.js
+++ components/section1.js
@@ -2,15 +2,34 @@ import Image from "next/image"
 import Link from "next/link"
 import Author from "./_child/author"
 import { Swiper, SwiperSlide } from 'swiper/react';
+import SwiperCore, { Autoplay } from 'swiper';
 import 'swiper/css';
 
+
+
 export default function section1() {
+    SwiperCore.use([Autoplay])
     return (
         <section className="py-16">
             <div className="container mx-auto md:px-20">
                 <h1 className="font-bold text-4xl pb-12 text-center">Trending</h1>
+                <Swiper
+                    slidesPerView={1}
+                    loop={true}
+                    autoplay={{
+                        delay: 2000
+                    }}
+                    onSlideChange={() => console.log('slide change')}
+                    onSwiper={(swiper) => console.log(swiper)}
+                >
+                    <SwiperSlide>{Slide()}</SwiperSlide>
+                    <SwiperSlide>{Slide()}</SwiperSlide>
+                    <SwiperSlide>{Slide()}</SwiperSlide>
+                    <SwiperSlide>{Slide()}</SwiperSlide>
 
-                {Slide()}
+                    ...
+                </Swiper>
+
 
 
             </div>
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,15 @@ import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import Format from '../layout/format'
 // components
-import Section1 from '../components/section1'; const inter = Inter({ subsets: ['latin'] })
+import Section1 from '../components/section1';
+import Section2 from '../components/section2';
+// const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
     <Format>
       <Section1></Section1>
+      <Section2></Section2>
     </Format>
   )
 }
