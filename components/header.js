4e5cca6eafe6c91a69ac5f2a0926a6ac056b52c3
ansharora28 <ansharorake2@gmail.com>
2023-04-17 23:38:21 +0530

    set up sanity

--- components/header.js
+++ components/header.js
@@ -3,12 +3,10 @@ export default function HomHeader() {
     return (
         <header className="bg-black-100">
             <div className="xl:container xl:mx-auto flex flex-col items-center sm:flex-row sm:justify-between text-center py-3">
-                <div className="md:flex-none w-96 order-2 sm:order-1 flex justify-center py-4 sm:py-0">
-                    <input type="text" className="mt-1 block w-60 px-3 py-2 bg-black border border-slate-300 rounded-full text-sm shadow-sm placeholder-slate-500 focus:outline-none focus:border-sky-500 focus:ring-1 focus:ring-sky-500 hover:bg-white text-black font-bold" placeholder="Search Bar under construction" />
-                </div>
+                
                 <div className="shrink w-40 sm:order-2 ">
                     <Link className='font-bold text-3xl' href={"/"}>
-                        <marquee> MY BLOG</marquee>
+                         MY BLOG
                     </Link>
                 </div>
             </div>
